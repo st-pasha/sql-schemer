2b235f37534c7ce8a574bec2e5d011dd0d0d5961
Pasha Stetsenko <stpasha@gmail.com>
2024-03-24 10:41:10 -0700

    fix newlines

--- tests/sql_parser.test.ts
+++ tests/sql_parser.test.ts
@@ -1,5 +1,5 @@
 import { describe, test, expect } from "bun:test";
-import { tokenize, Token } from "../src/sql_parser";
+import { tokenize } from "../src/sql_parser";
 
 describe("tokenize", () => {
   describe("Empty", () => {
